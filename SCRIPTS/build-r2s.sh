96e93b3eb0fb962621f442cdea46cddc2a90cc8b
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-09-08 21:24:28 +0800

    Update build-r2s.sh

--- SCRIPTS/build-r2s.sh
+++ SCRIPTS/build-r2s.sh
@@ -17,28 +17,28 @@ echo "添加 passwall2"
 echo "src-git passwall2 https://github.com/qqhpc/xiaorouji-openwrt-passwall2.git" >> feeds.conf.default
 
 
-echo "添加ssr-plus"
+echo "添加 helloworld"
 sed -i "/helloworld/d" "feeds.conf.default" && echo "src-git helloworld https://github.com/qqhpc/fw876-helloworld.git" >> feeds.conf.default
 # ssr-plus依赖sagernet-core,Sagernet内核和V2ray/Xray内核冲突
 
 echo "添加 openclash"
-echo "src-git OpenClash https://github.com/qqhpc/vernesong-OpenClash.git" >> feeds.conf.default
+echo "src-git OpenClash https://github.com/qqhpc/vernesong-OpenClash.git;dev" >> feeds.conf.default
 
-echo "下载feeds"
+echo "下载 feeds"
 ./scripts/feeds update -a
 
 echo "更新 go"
 rm -rf feeds/packages/lang/golang
 svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
 
-echo "安装feeds"
+echo "安装 feeds"
 ./scripts/feeds install -a
 
-echo "下载config"
+echo "下载 config"
 wget https://raw.githubusercontent.com/qqhpc/configfiles/main/openwrt/22.03/rk3328/NanoPi-R2S/r2s.fq.config.txt
 mv r2s.fq.config.txt .config
 
-echo "下载dl"
+echo "下载 dl"
 make download -j2
 
 echo "编译固件"
