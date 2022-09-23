c2f5ae4e5e42613c6dddf3e21c4b009d96b999c4
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-09-23 17:58:54 +0800

    Update build-lean-x86-bypass-gateway-openwrt.sh

--- SCRIPTS/build-lean-x86-bypass-gateway-openwrt.sh
+++ SCRIPTS/build-lean-x86-bypass-gateway-openwrt.sh
@@ -6,17 +6,20 @@ pwd
 cd /workdir/openwrt
 
 echo "添加 passwall"
-echo "src-git passwall https://github.com/qqhpc/xiaorouji-openwrt-passwall.git" >> feeds.conf.default
+git clone https://github.com/qqhpc/xiaorouji-openwrt-passwall.git ./package/passwall
 
 echo "添加 passwall2"
-echo "src-git passwall2 https://github.com/qqhpc/xiaorouji-openwrt-passwall2.git" >> feeds.conf.default
+git clone https://github.com/qqhpc/xiaorouji-openwrt-passwall2.git ./package/passwall2
 
 echo "添加 helloworld"
-sed -i "/helloworld/d" "feeds.conf.default" && echo "src-git helloworld https://github.com/qqhpc/fw876-helloworld.git" >> feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default" && git clone https://github.com/qqhpc/fw876-helloworld.git ./package/helloworld
 # ssr-plus依赖sagernet-core,Sagernet内核和V2ray/Xray内核冲突
 
 echo "添加 openclash"
-echo "src-git OpenClash https://github.com/qqhpc/vernesong-OpenClash.git;dev" >> feeds.conf.default
+svn export https://github.com/qqhpc/vernesong-OpenClash/branches/dev/luci-app-openclash ./package/openclash
+
+echo "添加 luci-app-adguardhome"
+git clone https://github.com/qqhpc/rufengsuixing-luci-app-adguardhome.git ./package/luciadguardhome
 
 echo "下载 feeds"
 ./scripts/feeds update -a
@@ -26,6 +29,9 @@ rm -rf feeds/packages/lang/golang
 svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
 
 echo "安装 feeds"
+./scripts/feeds install -a
+
+echo "安装 feeds again"
 ./scripts/feeds install -a
 
 echo "下载 config"
