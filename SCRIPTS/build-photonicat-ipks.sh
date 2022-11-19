422ec9b0c63c85f9318dda7b6d6fa3d05a422476
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-11-19 23:05:43 +0800

    Update build-photonicat-ipks.sh

--- SCRIPTS/build-photonicat-ipks.sh
+++ SCRIPTS/build-photonicat-ipks.sh
@@ -6,21 +6,21 @@ pwd
 cd /workdir/openwrt
 
 echo "添加 passwall"
-echo "src-git passwall_packages https://github.com/qqhpc/xiaorouji-openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git passwall_luci https://github.com/qqhpc/xiaorouji-openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 
 echo "添加 passwall2"
-echo "src-git passwall2 https://github.com/qqhpc/xiaorouji-openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 echo "添加 helloworld"
-sed -i "/helloworld/d" "feeds.conf.default" && echo "src-git helloworld https://github.com/qqhpc/fw876-helloworld.git;master" >> feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default" && echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 # ssr-plus依赖sagernet-core,Sagernet内核和V2ray/Xray内核冲突
 
 echo "添加 openclash"
-echo "src-git luciopenclash https://github.com/qqhpc/vernesong-OpenClash.git;master" >> feeds.conf.default
+echo "src-git luciopenclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
 
 echo "添加 luci-app-adguardhome"
-git clone https://github.com/qqhpc/rufengsuixing-luci-app-adguardhome.git ./package/luciadguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luciadguardhome
 
 echo "下载 feeds"
 ./scripts/feeds update -a
